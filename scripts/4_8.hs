02b5a1299eba97e72ff624c96ec5a738dd6ca180
Satoshi Tamaki <coela@Satoshis-Mac-mini.local>
2016-03-13 02:04:05 +0900

    fixed indent

--- scripts/4_8.hs
+++ scripts/4_8.hs
@@ -5,10 +5,10 @@ halve ns =  splitAt  ( (length ns) `div` 2) ns
 --2--
 safetail :: [a] -> [a]
 safetail as = if null as then [] 
-							else tail as
+              else tail as
 safetail2 :: [a] -> [a]
 safetail2 as | null as = []
-						 | otherwise = tail as
+             | otherwise = tail as
 safetail3 :: [a] -> [a]
 safetail3 [] = []
 safetail3 as = tail as
@@ -30,22 +30,22 @@ False `oror3` b = b
 
 oror4 :: Bool -> Bool -> Bool
 a `oror4` b | a == b = b
-						| otherwise = True
+            | otherwise = True
 
 --4--
 and2 :: Bool -> Bool -> Bool
 and2 a b = if  a then
-					   if b then
-						   True
-						 else
-						   False
-					 else
-					   False
+             if b then
+               True
+             else
+               False
+           else
+             False
 
 --5--
 and3 :: Bool -> Bool -> Bool
 and3 a b = if a  then b
-					 else False
+           else False
 
 --6--
 mult = (\x -> (\y -> (\z -> x * y * z) ) )
