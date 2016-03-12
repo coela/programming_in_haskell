4989f8f7a4aeb81d4627b6bc9422860a2c822a59
Satoshi Tamaki <coela@Satoshis-Mac-mini.local>
2016-03-12 23:32:05 +0900

    多分解けた

--- scripts/4_8.hs
+++ scripts/4_8.hs
@@ -1,19 +1,19 @@
+--1--
 halve :: [a] -> ([a],[a])
 halve ns =  splitAt  ( (length ns) `div` 2) ns
 
---a--
+--2--
 safetail :: [a] -> [a]
 safetail as = if null as then [] 
 							else tail as
---b--
 safetail2 :: [a] -> [a]
 safetail2 as | null as = []
 						 | otherwise = tail as
---c--
 safetail3 :: [a] -> [a]
 safetail3 [] = []
 safetail3 as = tail as
 
+--3--
 oror :: Bool -> Bool -> Bool
 True  `oror` True   = True
 True  `oror` False  = True
@@ -27,3 +27,25 @@ _ `oror2` _ = True
 oror3 :: Bool -> Bool -> Bool
 True `oror3` b = True
 False `oror3` b = b
+
+oror4 :: Bool -> Bool -> Bool
+a `oror4` b | a == b = b
+						| otherwise = True
+
+--4--
+and2 :: Bool -> Bool -> Bool
+and2 a b = if  a then
+					   if b then
+						   True
+						 else
+						   False
+					 else
+					   False
+
+--5--
+and3 :: Bool -> Bool -> Bool
+and3 a b = if a  then b
+					 else False
+
+--6--
+(\x -> (\y -> (\z -> x * y * z) ) )
